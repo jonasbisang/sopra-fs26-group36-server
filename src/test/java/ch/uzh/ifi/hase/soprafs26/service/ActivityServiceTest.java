db771a248e15b57701844f7f3a2146d4e3c5ae6d
Jonas Bisang <jonas.bisang@uzh.ch>
2026-04-24 11:03:14 +0200

    DevTask-#135 - Finished voting tests

--- src/test/java/ch/uzh/ifi/hase/soprafs26/service/ActivityServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs26/service/ActivityServiceTest.java
@@ -11,8 +11,12 @@ import org.springframework.web.server.ResponseStatusException;
 import ch.uzh.ifi.hase.soprafs26.entity.User;
 import ch.uzh.ifi.hase.soprafs26.repository.UserRepository;
 
+import ch.uzh.ifi.hase.soprafs26.repository.UnavailabilityRepository;
+
 import ch.uzh.ifi.hase.soprafs26.entity.Group;
 import ch.uzh.ifi.hase.soprafs26.repository.GroupRepository;
+
+
 
 import ch.uzh.ifi.hase.soprafs26.entity.Activity;
 import ch.uzh.ifi.hase.soprafs26.repository.ActivityRepository;
@@ -29,12 +33,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 
 
 public class ActivityServiceTest {
 
 	@Mock
     private ActivityRepository activityRepository;
+
+    @Mock
+    private UnavailabilityRepository unavailabilityRepository;
 
     @Mock
     private ActivityVoteRepository activityVoteRepository;
@@ -45,10 +53,14 @@ public class ActivityServiceTest {
     @Mock
     private GroupRepository groupRepository;
 
+    @InjectMocks
+    private ActivityService activityService;
+
 
     private Activity testActivity;
     private User testUser;
     private Group testGroup;
+
 
 
     @BeforeEach
@@ -74,7 +86,8 @@ public class ActivityServiceTest {
         Mockito.when(activityVoteRepository.existsByActivityIdAndUserId(Mockito.any(), Mockito.any())).thenReturn(false);
         Mockito.when(activityVoteRepository.save(Mockito.any())).thenReturn(new ActivityVote());
 	}
-}
+
+
 
 @Test
 public void testVoteSuccess() {
@@ -86,10 +99,53 @@ public void testVoteSuccess() {
     assertEquals(ActivityStatus.PENDING, testActivity.getStatus());
 }
 
-@Test
-
 
 @Test
+public void testMinimumNotReached() {
+    Mockito.when(activityVoteRepository.countByActivityIdAndWantsToJoinTrue(1L)).thenReturn(1L);
+    Mockito.when(activityRepository.save(Mockito.any())).thenReturn(testActivity);
+    activityService.vote(1L,1L,true,1L);
 
+    System.out.println("testVoteSuccess - The status is" + testActivity.getStatus());
+    assertEquals(ActivityStatus.PENDING, testActivity.getStatus());
+
+    Mockito.verify(activityRepository, Mockito.never()).save(Mockito.argThat(testing->testing.getStatus()
+    == ActivityStatus.SCHEDULED));
+    System.out.println("The scheduling has not been triggered because the minium participants count is not yet met");
+}
 
 @Test
+public void testTriggerSearch() {
+    Mockito.when(activityVoteRepository.countByActivityIdAndWantsToJoinTrue(1L)).thenReturn(2L);
+    Mockito.when(activityVoteRepository.findByActivityId(1L)).thenReturn(new ArrayList<>());
+    Mockito.when(activityRepository.save(Mockito.any())).thenReturn(testActivity);
+
+    activityService.vote(1L,1L,true,1L);
+    System.out.println("testTriggerSearch - The status is" + testActivity.getStatus());
+    assertEquals(ActivityStatus.SCHEDULED, testActivity.getStatus());
+}
+
+@Test
+public void testAlreadyVoted() {
+Mockito.when(activityVoteRepository.existsByActivityIdAndUserId(1L,1L)).thenReturn(true);
+
+ResponseStatusException fail = assertThrows(ResponseStatusException.class, () -> {
+    activityService.vote(1L, 1L, true, 1L);
+});
+System.out.println("TestAlreadyVoted - The status is" + fail.getStatusCode());
+    assertEquals(409, fail.getStatusCode().value());
+
+
+}
+
+@Test
+public void testActivityNotFound() {
+    Mockito.when(activityRepository.findById(99L)).thenReturn(Optional.empty());
+    ResponseStatusException fail = assertThrows(ResponseStatusException.class, () -> {activityService.vote(1L,99L,true, 1L);
+});
+System.out.println("testActivityNotFound - Result status:" + fail.getStatusCode());
+assertEquals(404, fail.getStatusCode().value()); 
+}
+
+
+}
